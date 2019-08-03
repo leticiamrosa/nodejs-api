ab5a9babbdd994a6152e52aa4b4d8e0b6ec9c1e0
leticiamrosa <leticiamonteiro.design@gmail.com>
2019-08-03 14:16:12 -0300

    verify if email already exists

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,17 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
+
   try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
+    }
     // pass all params for request in body
     const user = await User.create(req.body);
+
+    user.password = undefined;
+
     return res.send({ user });
   } catch (err) {
     return res.status(400).send({ error: "Registration Failed" });
